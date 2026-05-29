ec0e9aa31404fb88f6b1f7584cf8ee2774c92c82
hammikb <kaib1121@gmail.com>
2026-05-28 22:35:07 -0700

    fix: stopAll snapshot keys to avoid live-iterator mutation

--- src/main/monitor/MonitorEngine.js
+++ src/main/monitor/MonitorEngine.js
@@ -24,7 +24,7 @@ export class MonitorEngine extends EventEmitter {
   }
 
   stopAll() {
-    for (const id of this._timers.keys()) this.removeTask(id)
+    for (const id of [...this._timers.keys()]) this.removeTask(id)
   }
 
   getActiveTasks() {
